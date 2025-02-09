674214043cda8bf30950850772ed2489221ae853
Vladislav Yavorskij <yavorskij_vi@edu.surgu.ru>
2025-02-09 19:41:35 +0500

    seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
